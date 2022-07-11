c141cda958744f258da2d646650dd1d2caf618ed
KyoRuMan <ryokuman@naver.com>
2022-07-11 17:54:09 +0900

    first commit

---
+++ askcompany/askcompany/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path , include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog1/', include('blog1.urls')),
+    path('instagram/',include('instagram.urls')),
+    path('accounts/', include('accounts.urls'))
+]
